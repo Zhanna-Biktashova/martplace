6231af4190e5998a5f43d74b1dc7e23300baa323
Zhanna-Biktashova <biktashova.zhanna@mail.ru>
2020-09-17 00:00:26 +0500

    added new-items category-page

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,8 @@ $(function(){
     readOnly: true
   });
 
+  
+
   $('.product-slider__inner').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -19,6 +21,28 @@ $(function(){
     nextArrow: '<button class="slick-arrow slick-next"><img src="/images/icons/next.png" alt=""></button>',
     prevArrow: '<button class="slick-arrow slick-prev"><img src="/images/icons/prev.png" alt=""></button>',
   });
+
+  
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 500,
+    from: 30,
+    to: 300,
+    prefix: "$"
+});
+
+$('.icon-th-list').on('click', function(){
+  $('.products__item').addClass('list');
+  $('.icon-th-list').addClass('active');
+  $('.icon-th-large').removeClass('active');
+});
+
+$('.icon-th-large').on('click', function(){
+  $('.products__item').removeClass('list');
+  $('.icon-th-large').addClass('active');
+  $('.icon-th-list').removeClass('active');
+});
 
 
   var mixer = mixitup('.products__inner-box');
