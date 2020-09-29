dca5929a778c4bc1a945e2b5adfc3aff18e1f9dc
Zhanna-Biktashova <biktashova.zhanna@mail.ru>
2020-09-29 19:31:26 +0500

    added adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -45,6 +45,25 @@ $('.icon-th-large').on('click', function(){
 });
 
 
+$('.menu__btn').on('click', function(){
+  $('.menu__list').slideToggle();
+});
+
+
+$('.header__btn-menu').on('click', function(){
+  $('.header__box').toggleClass('active');
+});
+
+
+$('.single-product__tabs .tab').on('click', function(event) {
+  var id = $(this).attr('data-id');
+    $('.single-product__tabs').find('.tab-item').removeClass('active-tab').hide();
+    $('.single-product__tabs .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#'+id).addClass('active-tab').fadeIn();
+    return false;
+  });
+
   var mixer = mixitup('.products__inner-box');
   
 });
